030678acc2c2cbeb16006fe0c1d702d1bb0f44e0
nkowalchuk301 <nkowalchuk@hotmail.com>
2024-05-15 11:07:45 -0600

    Removed release from main

--- release/powersettings-USB.ps1
+++
@@ -1,9 +0,0 @@
-$powerManagement = Get-CimInstance -ClassName MSPower_DeviceEnable -Namespace root/WMI
-
-$usbDevices = Get-CimInstance -ClassName Win32_PnPEntity -Filter "PNPClass = 'USB'"
-
-$usbDevices | ForEach-Object {
-    $powerManagement | Where-Object InstanceName -Like ('*{0}*' -f $_.PNPDeviceID)
-} | Set-CimInstance -Property @{ Enable = $false }
-
-Write-Output "Power settings for USB devices have been disabled."
